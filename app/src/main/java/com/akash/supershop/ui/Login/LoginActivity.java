03f1dd1c167fe635089c9a3798fea13d52ab08a7
earhossain13@gmail.com <01955179507ab>
2019-12-24 16:17:18 +0600

    super shop complete(update)

--- app/src/main/java/com/akash/supershop/ui/Login/LoginActivity.java
+++ app/src/main/java/com/akash/supershop/ui/Login/LoginActivity.java
@@ -5,21 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.akash.supershop.R;
 import com.akash.supershop.ui.ShopOwner.ShopOwnerActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private Button loginbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email_edit_text);
+        password = findViewById(R.id.password_edit_text);
+        loginbtn = findViewById(R.id.loginbtn);
     }
 
     public void loginStudent(View view) {
-        Intent intent=new Intent(LoginActivity.this, ShopOwnerActivity.class);
-        startActivity(intent);
-        finish();
+        String emailid = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        Toast.makeText(LoginActivity.this,"success",Toast.LENGTH_SHORT).show();
+            if(emailid.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("1234")){
+                Intent intent=new Intent(LoginActivity.this, ShopOwnerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+
     }
 }
